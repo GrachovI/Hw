e5f0ad7f1f3fdd8efeb91804296e68365f2e2354
Ivan <ivangrache@gmail.com>
2022-12-03 11:19:23 +0200

    Version 3.2

--- Homework1.py
+++ Homework1.py
@@ -1,10 +1,18 @@
 class Animal:
-    species = "Tiger"
+    species = "Tigers"
 
     def __init__(self, power, age, weight):
         self.power = power #0-100
         self.age = age #years
         self.weight = weight #kg
+
+
+tiger = Animal(56, 12, 270)
+print(tiger.species)
+print(tiger.power)
+print(tiger.age)
+print(tiger.weight)
+print("")
 
 
 class Car:
@@ -15,3 +23,11 @@ class Car:
         self.age = age #years
         self.weight = weight #kg
         self.model = model
+
+
+car1 = Car(389, 4, 987)
+print(car1.producer)
+print(car1.max_speed)
+print(car1.age)
+print(car1.weight)
+print(car1.model)
